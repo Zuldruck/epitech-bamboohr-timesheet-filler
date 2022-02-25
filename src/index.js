ab861b7e0c938a7f081192ce8ded1dd2d2118312
Lucas Sanchez <lucas.sanchez@epitech.eu>
2022-02-25 12:39:22 +0100

    feat: implement compatibility for windows subsystem for linux

--- src/index.js
+++ src/index.js
@@ -34,7 +34,17 @@ function getActualMonthDaysOffset(previousPeriodForgotten = false)
 
 async function main()
 {
-  const browser = await puppeteer.launch();
+  const browser = await puppeteer.launch({
+    args: [
+      '--disable-gpu',
+      '--disable-dev-shm-usage',
+      '--disable-setuid-sandbox',
+      '--no-first-run',
+      '--no-sandbox',
+      '--no-zygote',
+      '--single-process',
+    ],
+  });
   const page = await browser.newPage();
   await page.goto('https://epitech.bamboohr.com/login.php');
 
